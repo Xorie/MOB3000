c35f823a8acc2f58b8d45e21e5d3a3f69fec9c7e
Daniel Thorbergsen <44589567+Danitho@users.noreply.github.com>
2020-11-27 11:50:24 +0100

    Oppdaterte notifikajsonsfunksjonalitet
    Signed-off-by: Daniel Thorbergsen <44589567+Danitho@users.noreply.github.com>

--- app/src/main/java/com/example/mob3000/BrukerProfil.java
+++ app/src/main/java/com/example/mob3000/BrukerProfil.java
@@ -1,48 +1,55 @@
 package com.example.mob3000;
 
-
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Looper;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.view.menu.MenuAdapter;
+import androidx.cardview.widget.CardView;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.room.DatabaseConfiguration;
-import androidx.room.InvalidationTracker;
-import androidx.sqlite.db.SupportSQLiteOpenHelper;
-
 import java.io.Serializable;
 import java.util.List;
 
 public class BrukerProfil extends AppCompatActivity {
     private MyDatabase mDb;
     private StudentAdapter mAdapter;
-    private Button invite;
     private RecyclerView recView;
-
-    // For notifikasjonsklokka:
+    private Button invite;
     private TextView num;
     private final int MAX_NUM = 99;
-    private int notification_num_count = 1;
+    private int notification_num_count = 0;
+    private CardView notification_dot;
+    private ImageView notification_bell;
 
+    // wip
+    private String valgt_emne;
+    private String valgt_gruppe;
+    private String kommentar;
+    public String getEmne() {
+        return valgt_emne;
+    }
+    public String getGruppe() {
+        return valgt_gruppe;
+    }
+    public String getKommentar() {
+        return kommentar;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,52 +61,86 @@ public class BrukerProfil extends AppCompatActivity {
         mAdapter = new StudentAdapter(this);
         recView.setAdapter(mAdapter);
 
+        createNotificationChannel();
+        NotificationCompat.Builder build = new NotificationCompat.Builder(this, "wtf")
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle("Studentbay")
+                .setContentText("du har fått en invitasjon")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        notification_dot = findViewById(R.id.notification_dot);
+        notification_bell = findViewById(R.id.notification_bell);
+
+        if(notification_num_count == 0){
+            notification_dot.setVisibility(View.INVISIBLE);
+        }
+
+        notification_bell.setOnClickListener(v -> {
+            notification_num_count = 0;
+            notification_dot.setVisibility(View.INVISIBLE);
+            AlertDialog.Builder builder = new AlertDialog.Builder(BrukerProfil.this);
+            builder.setTitle("Invitasjon");
+            View myView = getLayoutInflater().inflate(R.layout.activity_notification, null);
+            builder.setNegativeButton("Avslå", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(BrukerProfil.this, "Avslått",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            builder.setPositiveButton("Godta", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(BrukerProfil.this, "Godtatt",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            builder.setView(myView);
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        });
+
         num = findViewById(R.id.notification_num);
         invite = findViewById(R.id.invite);
-        invite.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(BrukerProfil.this);
-                View myView = BrukerProfil.this.getLayoutInflater().inflate(R.layout.dialog_invite, null);
+        invite.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(BrukerProfil.this);
+            View myView = getLayoutInflater().inflate(R.layout.dialog_invite, null);
 
-                final Spinner emner = myView.findViewById(R.id.listeEmner);
-                final Spinner grupper = myView.findViewById(R.id.listeGrupper);
-                final EditText user_input = myView.findViewById(R.id.userinput);
+            final Spinner emner = myView.findViewById(R.id.listeEmner);
+            final Spinner grupper = myView.findViewById(R.id.listeGrupper);
+            final EditText user_input = myView.findViewById(R.id.userinput);
 
-                ArrayAdapter<String> adapter1 = new ArrayAdapter<>(BrukerProfil.this,
-                        android.R.layout.simple_spinner_item,
-                        BrukerProfil.this.getResources().getStringArray(R.array.course_list));
-                adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            ArrayAdapter<String> adapter1 = new ArrayAdapter<>(BrukerProfil.this,
+                    android.R.layout.simple_spinner_item,
+                    getResources().getStringArray(R.array.course_list));
+            adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-                ArrayAdapter<String> adapter2 = new ArrayAdapter<>(BrukerProfil.this,
-                        android.R.layout.simple_spinner_item,
-                        BrukerProfil.this.getResources().getStringArray(R.array.group_list));
-                emner.setAdapter(adapter1);
-                grupper.setAdapter(adapter2);
+            ArrayAdapter<String> adapter2 = new ArrayAdapter<>(BrukerProfil.this,
+                    android.R.layout.simple_spinner_item,
+                    getResources().getStringArray(R.array.group_list));
+            emner.setAdapter(adapter1);
+            grupper.setAdapter(adapter2);
 
-                // Knapp i dialog-boksen:
-                builder.setPositiveButton("Send invitasjon", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(BrukerProfil.this, "Invitasjon sendt",
-                                Toast.LENGTH_SHORT).show();
-
-                        // Foreløpig sendes det bare en push-varsel til brukeren selv.
-                        // Skal også vise notifikasjonsvarsel eller øke varsleren per invitasjon...
-                        increaseNum();
-                        pushNotification(); // WIP
-
-
-                        // Her må verdiene (som bruker har valgt) sendes videre til den brukeren
-                        // som får invitasjon.
-
-                    }
-                });
-
-                builder.setView(myView);
-                AlertDialog dialog = builder.create();
-                dialog.show();
-            }
+            builder.setPositiveButton("Send invitasjon", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(BrukerProfil.this, "Invitasjon sendt",
+                            Toast.LENGTH_SHORT).show();
+                    increaseNum();
+                    notificationManager.notify(100, build.build());
+                    // wip
+                    valgt_emne = emner.getSelectedItem().toString();
+                    valgt_gruppe = grupper.getSelectedItem().toString();
+                    kommentar = user_input.getText().toString();
+                    //
+                }
+            });
+            builder.setView(myView);
+            AlertDialog dialog = builder.create();
+            dialog.show();
         });
     }
 
@@ -108,7 +149,6 @@ public class BrukerProfil extends AppCompatActivity {
         super.onResume();
         showListData();
     }
-
 
     private void showListData(){
         AppExecutors.getInstance().diskIO().execute(new Runnable() {
@@ -131,46 +171,6 @@ public class BrukerProfil extends AppCompatActivity {
         });
     }
 
-    // Metode for å øke antallet på notifikasjonsklokka
-    public void increaseNum(){
-        notification_num_count++;
-        if(notification_num_count > MAX_NUM){
-            Log.d("Counter", "Maks antall nådd!");
-        }
-        else {
-            num.setText(String.valueOf(notification_num_count));
-        }
-    }
-
-    // Metode for push-varsel
-    public void pushNotification(){
-        String melding = "Du har blitt invitert av [] til gruppe [], MOB3000";
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(
-                BrukerProfil.this
-        )
-                .setSmallIcon(R.drawable.ic_notification_message)
-                .setContentTitle("New notification")
-                .setContentText(melding)
-                .setAutoCancel(true);
-
-        Intent intent = new Intent(BrukerProfil.this,
-                Notification.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        intent.putExtra("Melding:", melding);
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(BrukerProfil.this,
-                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        builder.setContentIntent(pendingIntent);
-        NotificationManager notificationManager = (NotificationManager)getSystemService(
-                Context.NOTIFICATION_SERVICE
-        );
-
-        notificationManager.notify(0, builder.build());
-
-    }
-
-
     // om bruker ønsker å slette profilen sin
     public void onClickDelete(View view) {
             new Thread(new Runnable() {
@@ -188,7 +188,6 @@ public class BrukerProfil extends AppCompatActivity {
                     startActivity(intent);
                 }
         }).start();
-
     }
 
     public void onClickUpdate(View view) {
@@ -204,5 +203,24 @@ public class BrukerProfil extends AppCompatActivity {
         final String bruker = i.getStringExtra("SID");
         Intent intent = (new Intent(this, OmAppen.class).putExtra("ID", (Serializable) bruker_liste).putExtra("SID", bruker));
         startActivity(intent);
+    }
+
+    private void createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= 26) {
+            CharSequence name = "testChannel";
+            String desc = "Testing description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("wtf", name, importance);
+            channel.setDescription(desc);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    public void increaseNum(){
+        notification_num_count++;
+        if(notification_num_count > 0){ notification_dot.setVisibility(View.VISIBLE); }
+        if(notification_num_count > MAX_NUM){ Log.d("Counter", "Maks antall nådd!"); }
+        else num.setText(String.valueOf(notification_num_count));
     }
 }
